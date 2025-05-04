daa46da462e8d279e38a1be6bc47a7d4547ae356
marselin4 <rodulfcristmas@gmail.com>
2025-05-04 15:44:26 +0000

    Add files via upload

---
+++ polybar/GETREADYTORUMBLE.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+ killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do
+sleep 0.2; done
+
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini bar1 &
+polybar -c ~/.config/polybar/config.ini bar2 &
+polybar -c ~/.config/polybar/config.ini bar3 &
+polybar -c ~/.config/polybar/config.ini bar4 &
+echo "Bars launched..."
+disown
